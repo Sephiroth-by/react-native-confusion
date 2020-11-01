b172753ed00f90cae704c562aedf1b0fd116f3ac
Oleg Ilenkov <oleg.ilenkov@oracle.com>
2020-11-01 22:26:26 +0300

    add secure store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,7 @@ import { DISHES } from '../shared/dishes';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 
 const mapStateToProps = state => {
   return {
@@ -97,6 +98,23 @@ const FavoritesNavigator = createStackNavigator({
       onPress={ () => navigation.navigate('DrawerToggle') } />    
   })
 })
+
+const LoginNavigator = createStackNavigator({
+  Login: { screen: Login }
+}, {
+navigationOptions: ({ navigation }) => ({
+  headerStyle: {
+      backgroundColor: "#512DA8"
+  },
+  headerTitleStyle: {
+      color: "#fff"            
+  },
+  headerTintColor: "#fff",
+  headerLeft: <Icon name="menu" size={24}
+    iconStyle={{ color: 'white' }} 
+    onPress={ () => navigation.toggleDrawer() } />    
+})
+});
 
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
@@ -200,10 +218,26 @@ const MainNavigator = createDrawerNavigator({
               />
             ),
           }
-        }
+        },
+      Login: 
+        { screen: LoginNavigator,
+          navigationOptions: {
+            title: 'Login',
+            drawerLabel: 'Login',
+            drawerIcon: ({ tintColor, focused }) => (
+              <Icon
+                name='sign-in'
+                type='font-awesome'            
+                size={24}
+                iconStyle={{ color: tintColor }}
+              />
+            ),
+          }
+        },
   }, {
-drawerBackgroundColor: '#D1C4E9',
-contentComponent: CustomDrawerContentComponent
+    initialRouteName: 'Home',
+    drawerBackgroundColor: '#D1C4E9',
+    contentComponent: CustomDrawerContentComponent
 });
 
 const App = createAppContainer(MainNavigator);
